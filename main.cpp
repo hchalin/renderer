2121097c85d0f3ff640aa658b18998da2561b2a6
Hayden <hchalin98@gmail.com>
2024-06-19 09:15:19 -0600

    Renderer now uses rays.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 color ray_color(const ray & r){
-  return color(0,0,0);
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0); // lerp function
 }
 
 int main() {
@@ -40,7 +43,11 @@ int main() {
   for (int j = 0; j < image_height; j++) {
       std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
       for (int i = 0; i < image_width; i++) {
-        auto pixel_color =  color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+        auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+        auto ray_direction =  pixel_center - camera_center;
+        ray r(camera_center, ray_direction); // create a origin and direction
+
+        color pixel_color = ray_color(r); // ray color is defined above
         write_color(std::cout, pixel_color);
     }
       
