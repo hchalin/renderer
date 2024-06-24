30618e9f62d111051bfb4ea60935fe0abae371fa
Hayden <hchalin98@gmail.com>
2024-06-24 09:35:21 -0700

    working on adding a sphere to the renderer. Renderer is creating errors with the PPM image file.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "color.h"
 #include "ray.h"
 
+//bool hit_sphere(const point3 & center, double radius, const ray & r){
+  //vec3 oc = center - r.origin();
+  //auto a = dot(r.direction(), r.direction());
+  //auto b = -2.0 * dot(r.direction(), oc);
+  //auto c = dot(oc, oc) - radius*radius;
+  //auto discriminant = b*b - 4*a*c;
+  //return (discriminant >= 0);
+
+//}
+
 color ray_color(const ray & r){
+  //if (hit_sphere(point3(0,0,-1), 0.5, r))
+    //return color(0, 1, 0);
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5*(unit_direction.y() + 1.0);
   return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0); // lerp function
@@ -37,11 +50,11 @@ int main() {
                             - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
   auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-    // Render
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    // render
+  std::cout << "p3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
-      std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+      std::clog << "\rscanlines remaining: " << (image_height - j) << ' ' << std::flush;
       for (int i = 0; i < image_width; i++) {
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto ray_direction =  pixel_center - camera_center;
