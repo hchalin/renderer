1c6c9eaf958b410df8f927cd81fcab492e5816e8
Hayden <hchalin98@gmail.com>
2024-09-11 11:30:10 -0700

    color error fixed

--- hittable_list.h
+++ hittable_list.h
@@ -1,34 +1,39 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
-// file include
 #include "hittable.h"
 
-// Preproccesor
 #include <vector>
-#include <memory>
 
-// namespaces
-using std::make_shared;
-using std::shared_ptr; // smart pointer
 
 class hittable_list : public hittable {
-
   public:
     std::vector<shared_ptr<hittable>> objects;
 
     hittable_list() {}
     hittable_list(shared_ptr<hittable> object) { add(object); }
 
-    void clear(){objects.clear();}
+    void clear() { objects.clear(); }
 
-    void add(shared_ptr<hittable> object){
-      objects.push_back(object);
+    void add(shared_ptr<hittable> object) {
+        objects.push_back(object);
     }
 
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_tmax;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
 };
 
-
-
-
-#endif HITTABLE_LIST_H
+#endif
