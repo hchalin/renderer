a71d7811b90c9a70d88fdb22ebd0ad7f8b349478
Hayden <hchalin98@gmail.com>
2024-09-16 11:07:36 -0700

    added difuse lighting

--- main.cpp
+++ main.cpp
@@ -6,26 +6,19 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
-
-
-
-
 // FIXME: there is a problem in the main fn
-int main() {
+int main()
+{
   hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    camera cam;
+  camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
-    cam.samples_per_pixel = 100;
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
 
-    cam.render(world);
+  cam.render(world);
 }
-
-
-
-
